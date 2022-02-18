e174a3e0b8fb84f69c2818aa23f00142b8afcdc8
Aren <34808599+bm9520@users.noreply.github.com>
2022-02-18 21:28:14 +0900

    Update send_to_weather.py

--- send_to_weather.py
+++ send_to_weather.py
@@ -55,3 +55,18 @@ except Exception as e:
 finally:
     print("finally...")
     driver.quit()
+    
+img_array = []
+for filename in glob.glob('kweather.png'):
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+
+
+out = cv2.VideoWriter('/docs/project.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 60, size)
+
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
+
