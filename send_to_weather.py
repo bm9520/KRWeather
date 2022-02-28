c6176300aad6bd13aa26543a568629e93eaeb1dc
Aren <34808599+bm9520@users.noreply.github.com>
2022-02-28 17:39:50 +0900

    Update send_to_weather.py

--- send_to_weather.py
+++ send_to_weather.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
 from io import BytesIO
 import requests
@@ -25,6 +27,8 @@ try:
     # 기상청 접속
     driver.get('https://m.kma.go.kr/m/nation/forecast.jsp?ampm=1')
     driver.maximize_window()
+    
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'nation_map posi2')))
 
     kweather_map = driver.find_element(By.CLASS_NAME,"nation_map posi2")
 
