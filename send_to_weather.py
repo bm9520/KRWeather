a32bf93dbfea31f82a1add7fb8472404699b3853
Aren <34808599+bm9520@users.noreply.github.com>
2022-03-01 15:23:45 +0900

    Update send_to_weather.py

--- send_to_weather.py
+++ send_to_weather.py
@@ -62,7 +62,7 @@ try:
             img_array.append(img)
 
 
-        out = cv2.VideoWriter('./kweather.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 60, size)
+        out = cv2.VideoWriter('kweather.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 60, size)
 
         for i in range(len(img_array)):
             out.write(img_array[i])
