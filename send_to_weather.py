fc8db15c0556a75e63ad6259f0ae197dc8712bb3
Aren <34808599+bm9520@users.noreply.github.com>
2022-06-17 17:53:39 +0900

    Update send_to_weather.py

--- send_to_weather.py
+++ send_to_weather.py
@@ -19,7 +19,7 @@ try:
     options.add_argument("start-maximized")
     options.add_argument("lang=ko_KR")
     options.add_argument('headless')
-    options.add_argument('window-size=410x700')
+    options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
     options.add_argument("--no-sandbox")
 
